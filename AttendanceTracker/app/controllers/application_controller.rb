248b345e5c6edaaf67ca2da853029734632c92a7
Korey Heiser <kheiser@uncc.edu>
2019-04-07 22:15:28 +0000

    Log in pages redirect to their proper home page based on student/teacher status

--- AttendanceTracker/app/controllers/application_controller.rb
+++ AttendanceTracker/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+   
+    private
+    def after_sign_in_path_for(resource)
+        if current_user.teacher?
+        teacher_attendance_index_path
+            
+        else
+         student_attendance_index_path
+        end
+     
+    end
 end
