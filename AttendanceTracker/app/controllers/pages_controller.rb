2d1735e87900cda6a616804982c425f4a6a5122e
Korey Heiser <kheiser@uncc.edu>
2019-04-07 22:37:10 +0000

    Set up redirect for home page when logged in

--- AttendanceTracker/app/controllers/pages_controller.rb
+++ AttendanceTracker/app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def home
+    if user_signed_in?
+      if current_user.teacher?
+       redirect_to teacher_attendance_index_path
+            
+        else
+        redirect_to student_attendance_index_path
+      end
+    end
   end
 end
--- AttendanceTracker/app/controllers/welcome_controller.rb
+++ AttendanceTracker/app/controllers/welcome_controller.rb
@@ -1,6 +1,13 @@
 class WelcomeController < ApplicationController
   def index
-    
+    if user_signed_in?
+      if current_user.teacher?
+       redirect_to teacher_attendance_index_path
+            
+        else
+        redirect_to student_attendance_index_path
+      end
+    end
   end
   
 end
