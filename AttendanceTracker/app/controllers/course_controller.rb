6615d3db249abda9f674a675876b582c8376b10b
Korey Heiser <kheiser@uncc.edu>
2019-04-12 22:30:05 +0000

    Added methods to course controller

--- AttendanceTracker/app/controllers/course_controller.rb
+++ AttendanceTracker/app/controllers/course_controller.rb
@@ -1,2 +1,31 @@
 class CourseController < ApplicationController
+    def new
+        @course = Course.new
+       
+    end
+    
+    def create
+    end 
+    
+    def delete
+       Course.find(params[:name]).destroy 
+    end
+    
+    def list
+        @course = Course.all
+    end
+    
+    def show
+        @course = Course.find(params[:name])
+    end 
+    
+    def edit
+        @course = Course.find(params[:name])
+    end 
+    
+    
+    def course_params
+       params.require(:course).permit(:name, :courseID, :registrationID) 
+    end
+    
 end
--- AttendanceTracker/db/migrate/create_courses.rb
+++ AttendanceTracker/db/migrate/create_courses.rb
@@ -2,8 +2,10 @@ class CreateCourses < ActiveRecord::Migration[5.2]
   def change
     create_table :courses do |t|
       t.text :name
+      t.text :teacher
       t.text :courseID
       t.text :registrationID
+      
 
       t.timestamps
     end
