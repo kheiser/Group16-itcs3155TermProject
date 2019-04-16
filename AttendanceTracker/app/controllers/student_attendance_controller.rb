284faac922959215dd7934ad0bb3e5049d6a2ac0
Eric Ung <eung@uncc.edu>
2019-04-16 01:40:00 +0000

    Added CRUD functionality to teacher and student

--- AttendanceTracker/app/controllers/student_attendance_controller.rb
+++ AttendanceTracker/app/controllers/student_attendance_controller.rb
@@ -1,10 +1,23 @@
 class StudentAttendanceController < ApplicationController
     def new
-    
+       @student = Student.new
     end 
     
     def create
+        render plain: params[:student].inspect
     end
+    
+    def delete
+        Student.find(params[:name]).destroy
+    end
+    
+    def show
+        @student = Student.find(params[:name])
+    end
+    
+    def edit
+        @student = Student.find(params[:name])
+    end 
     
     
     
--- AttendanceTracker/app/controllers/teacher_attendance_controller.rb
+++ AttendanceTracker/app/controllers/teacher_attendance_controller.rb
@@ -1,7 +1,21 @@
 class TeacherAttendanceController < ApplicationController
     def new
+        @teacher = Teacher.new
     end 
     
     def create
+        render plain: params[:teacher].inspect
     end
+    
+    def delete
+        Teacher.find(params[:name]).destroy
+    end
+    
+    def show
+        @teacher = Teacher.find(params[:name])
+    end
+    
+    def edit
+        @teacher = Teacher.find(params[:name])
+    end 
 end
