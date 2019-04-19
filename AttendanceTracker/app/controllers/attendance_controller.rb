fcd95a3d836de6f7011c0812d574f5c9afc5be32
Korey Heiser <kheiser@uncc.edu>
2019-04-19 00:08:02 +0000

    added absence to view in course page

--- AttendanceTracker/app/controllers/attendance_controller.rb
+++ AttendanceTracker/app/controllers/attendance_controller.rb
@@ -1,12 +1,22 @@
 class AttendanceController < ApplicationController
     
     def create
-        @user = User.find(params[:email])
-        @attendance = Attendance.new
+        @course = Course.find(params[:name])
+        @attendance = @course.attendance.create(attendance_params)
+        redirect_to course_path(@course)
     end
     
     def destroy
        @user = User.find(params[:email])
        @attendance.destroy
+    end
+    
+    def index
+    @attendance = Attendance.all
+    end
+    
+    private
+    def attendance_params
+       params.require(:attendance).permit(:student, :absences) 
     end
 end
